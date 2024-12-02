c084d270970a06dbc2cfa098b78535ca3552a748
OBSoft-Berkaycel <berkaycel@gmail.com>
2024-12-02 19:28:13 +0300

    Feature: Flowbite package was installed and a basic admin dashboard layout was added.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\RedirectAdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +26,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+## Admin Auth Routes
+Route::middleware(RedirectAdminMiddleware::class)->prefix('admin')->group(function(){
+    Route::get('login',[AdminAuthController::class,'showLoginForm'])->name('admin.login');
+    Route::post('login',[AdminAuthController::class,'login'])->name('admin.login');
+    Route::post('logout', [AdminAuthController::class,'logout'])->name('admin.logout');
+});
+
+
+## Admin Routes
+Route::middleware(['auth',AdminMiddleware::class])->prefix('admin/v1')->group(function(){
+    Route::get('dashboard',[AdminController::class,'index'])->name('admin.dashboard');
 });
 
 require __DIR__.'/auth.php';
